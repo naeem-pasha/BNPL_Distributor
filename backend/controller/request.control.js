ad2cd6a624724e6e08420089a4aaf5ab34fb970e
kashan iqbal <kashaniqbal33@gmail.com>
2025-04-28 15:51:08 +0500

    fix_cookies_backend_issue

--- backend/controller/request.control.js
+++ backend/controller/request.control.js
@@ -108,7 +108,7 @@ const loginDistributer = async (req, res) => {
     const cookieOptions = {
       expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      secure: true,
+      secure: false,
     };
 
     res.cookie("token", token, cookieOptions);
